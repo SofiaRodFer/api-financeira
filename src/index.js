06add28a1c9421f8d50df1f751175a1931dc2b39
Sofia <sofiarodrigues45@gmail.com>
2021-10-28 18:52:57 -0300

    Implementando função de extrato por data

--- src/index.js
+++ src/index.js
@@ -93,4 +93,19 @@ app.post('/withdraw', verifyIfAccountExistsCPF, (req, res) => {
     return res.status(201).send()
 })
 
+app.get('/statement/date', verifyIfAccountExistsCPF, (req, res) => {
+    const { customer } = req
+    const { date } = req.query
+
+    const dateFormat = new Date(date + ' 00:00')
+
+    const statement = customer.statement.filter(
+        (statement) => 
+            statement.created_at.toDateString() === 
+            new Date(dateFormat).toDateString()
+    )
+
+    return res.json(statement)
+})
+
 app.listen('3333')
