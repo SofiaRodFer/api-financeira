c713bc82f10d840446610fe65b0d7d7ce4ec8074
Sofia <sofiarodrigues45@gmail.com>
2021-10-28 18:33:03 -0300

    Implementando operação de saque

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const { response } = require('express');
-const express = require('express')
+const express = require('express');
+const { reset } = require('nodemon');
 const { v4: uuidv4 } = require('uuid')
 
 const app = express()
@@ -17,6 +18,18 @@ function verifyIfAccountExistsCPF(req, res, next) {
 
     req.customer = customer
     return next()
+}
+
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type === 'credit') {
+            return acc + operation.amount
+        } else {
+            return acc - operation.amount
+        }
+    }, 0)
+
+    return balance
 }
 
 app.post('/account', (req, res) => {
@@ -56,6 +69,25 @@ app.post('/deposit', verifyIfAccountExistsCPF, (req, res) => {
         type: "credit"
     }
 
+    customer.statement.push(statementOperation)
+
+    return res.status(201).send()
+})
+
+app.post('/withdraw', verifyIfAccountExistsCPF, (req, res) => {
+    const { amount } = req.body
+    const { customer } = req
+
+    const balance = getBalance(customer.statement)
+    if(balance < amount) {
+        return res.status(400).json({error: "Insufficient funds"})
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit"
+    }
     customer.statement.push(statementOperation)
 
     return res.status(201).send()
