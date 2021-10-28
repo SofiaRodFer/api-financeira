247ce3b846a4ace6625c3931e9a485a44b7bab57
Sofia <sofiarodrigues45@gmail.com>
2021-10-28 18:18:57 -0300

    Criando opção de depósito

--- src/index.js
+++ src/index.js
@@ -45,4 +45,20 @@ app.get('/statement', verifyIfAccountExistsCPF, (req, res) => {
     return res.json(customer.statement)
 })
 
+app.post('/deposit', verifyIfAccountExistsCPF, (req, res) => {
+    const { description, amount } = req.body
+    const { customer } = req
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation)
+
+    return res.status(201).send()
+})
+
 app.listen('3333')
